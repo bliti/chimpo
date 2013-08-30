5ebf1fdf903ed0507579b4cd9f0f868f71f2ef58
bliti <pablo.rivera.programmer@gmail.com>
2013-08-30 00:14:53 -0400

    JS is tested and works as required.

--- chimpo.js
+++ chimpo.js
@@ -14,14 +14,13 @@ var chimpoBar = '\<div style="' +
 				'background: red;' +
 				'padding-left: 100px;' +
 				'padding-top: 10px;">' + 
-				
-				'\<form id="chimpoBar" action="#" method="GET">' +
-
-                '\<input type="email" required>'+
-				
-				'\<button type="submit" id="chimpoSubmit">Subscribe to my newsletter\</button>' +
-				
-				'\</form>' +
+			
+				'<form class="chimpoForm" id="chimpoForm" action="http://github.us7.list-manage1.com/subscribe/post" method="POST">' +
+				 ' <input type="hidden" name="u" value="fab15eaf19084212d78240d70">' +
+				 ' <input type="hidden" name="id" value="6b08a63dfd">' +
+				 ' <input class="input" type="email" value="" name="MERGE0" placeholder="Your Email Address" required>' +
+				 ' <input type="submit" value="Send" name="submit" id="mc-embedded-subscribe">' +
+				 '</form>' +
 
 				'\</div>';
 
@@ -30,13 +29,30 @@ var chimpoBar = '\<div style="' +
 $(document).ready(function() {
 	$('body').append(chimpoBar);
 	
-	$('#chimpoSubmit').click(function() {
-		alert('hello');
-		});
+	$('#chimpoForm').submit(function(e) {
+	  var $this = $(this);
+	  $.ajax({
+	      type: "POST", // GET & url for json slightly different
+	      url: "http://github.us7.list-manage1.com/subscribe/post-json?c=?",
+	      data: $this.serialize(),
+	      dataType    : 'json',
+	      contentType: "application/json; charset=utf-8",
+	      error       : function(err) { alert("Could not connect to the registration server."); },
+	      success     : function(data) {
+	          if (data.result != "success") {
+				  alert('Shit broken');
+	          } else {
+				  alert('Worked');
+	          }
+	      }
+	  });
+	  return false;
+	});
 	});
 	
 	
 	//TODO:
-	// add validation
-	// add ajax POST call
-	// mailchimp specific data
+	// fix html
+	// add validation * IN PROGRESS
+	// add ajax POST call * DONE
+	// mailchimp specific data * DONE
